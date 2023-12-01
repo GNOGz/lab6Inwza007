0f051345981a4fef2b09fb10d9088e6b2ab95b70
GNOGz <gong6321@gmail.com>
2023-12-01 16:04:23 +0700

    AddAllFuncion

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -2,12 +2,23 @@
 #include <cmath>
 using namespace std;
 double deg2rad(double deg){
-    return deg*180/M_PI;
+    return deg*180.0/M_PI;
 }
 double rad2deg(double rad){
-    return rad*M_PI/180;
+    return rad*M_PI/180.0;
 }
+double findXComponent(double vecX1,double degX1,double vecX2,double degX2){
+    return ;
+}
+double findYComponent(double vecY1,double degY1,double vecY2,double degY2){
+    return ;
+}
+double pythagoras(double xComp,double yComp){
+    return sqrt(pow(xComp,2)+pow(yComp,2));
+}
+void showResult(){
 
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -26,6 +37,5 @@ int main(){
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
-
     showResult(result_vec_length,result_vec_direction);
 }
