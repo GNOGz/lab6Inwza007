8151f20730fc299db692715f3fec65a783deefde
GNOGz <gong6321@gmail.com>
2023-12-01 15:40:12 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 int main(){
+    int n ;
+    int even = 0,odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    cout << "Hello world";
+    cin >> n ;
+
+    while(n != 0){
+        if (n % 2 == 0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+
+    cout << "Enter an integer: ";
+    cin >> n ;
+    }
+    cout << "#Even numbers = "<< even<<"\n" ;
+    cout << "#Odd numbers = " << odd ;
     return 0;
 }
